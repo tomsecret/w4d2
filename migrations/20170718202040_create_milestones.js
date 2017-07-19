9f954a140ca724c7f73f8a582dde829b4973d8c2
tomsecret <bladelun@gmail.com>
2017-07-19 01:33:12 +0000

    added foreign key

--- migrations/20170718202040_create_milestones.js
+++ migrations/20170718202040_create_milestones.js
@@ -14,3 +14,18 @@ exports.down = function(knex, Promise) {
     knex.schema.dropTable('milestones')
   ])
 };
+
+// exports.up = function(knex, Promise) {
+//   return Promise.all([
+//     knex.schema.table('milestones', function(table){
+//       table.integer('famous_people_id');
+//       table.foreign('famous_people_id').references('famous_people.id')
+//     })
+//   ])
+// };
+
+// exports.down = function(knex, Promise) {
+//   return Promise.all([
+//     knex.schema.dropTableIfExists('milestones')
+//   ])
+// };
---
+++ migrations/20170718211635_add_famous_ppl_id.js
@@ -0,0 +1,30 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.integer('famous_people_id');
+      table.foreign('famous_people_id').references('famous_people.id')
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('milestones')
+  ])
+};
+
+// exports.up = function(knex, Promise) {
+//   return Promise.all([
+//     knex.schema.createTable('milestones', function(table){
+//       table.string('description');
+//       table.date('date_achieved');
+//       table.integer('id');
+//     })
+//   ])
+// };
+
+// exports.down = function(knex, Promise) {
+//   return Promise.all([
+//     knex.schema.dropTable('milestones')
+//   ])
+// };
